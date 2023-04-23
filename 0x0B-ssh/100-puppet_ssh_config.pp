0d4aac927f16d3ee2c9b06e63da1117e015c36e3
ronkybabe <ronkeashaju@gmail.com>
2023-04-23 16:08:22 -0700

    task 5

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+#Use the private key
+exec { 'Turn off password auth':
+  command => 'bash -c "echo PasswordAuthentication no >> /etc/ssh/ssh_config"',
+  path    => '/usr/bin:/usr/sbin:/bin'
+}
+exec { 'Declare identity file':
+  command => 'bash -c "echo IdentityFile \'~/.ssh/school\' >> /etc/ssh/ssh_config"',
+  path    => '/usr/bin:/usr/sbin:/bin'
+}
+exec { 'Turn on pubkey auth':
+  command => 'bash -c "echo PubkeyAuthentication yes >> /etc/ssh/ssh_config"',
+  path    => '/usr/bin:/usr/sbin:/bin'
+}
