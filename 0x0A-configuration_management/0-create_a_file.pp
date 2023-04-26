045b88ad730dc4951e45592442edc5b03ec40ea2
ronkybabe <ronkeashaju@gmail.com>
2023-04-26 02:18:11 -0700

    task 0

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,10 @@
 # Using Puppet create a file in/tmp
 
-file {'graceland':
- path      => '/tmp/graceland',
- mode      => '0744',
- owner     => 'www-data',
- group     => 'www-data',
- content   => 'I love Puppet'
+file {'school':
+  path      => '/tmp/school',
+  mode      => '0744',
+  owner     => 'www-data',
+  group     => 'www-data',
+  content   => 'I love Puppet',
 }
 
