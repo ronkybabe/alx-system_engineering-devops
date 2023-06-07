b71e7012bebf1f6eb83e3b047cc0defec8888d3f
Ronke Ashaju <ronkeashaju@gmail.com>
2023-06-07 13:51:24 +0000

    task 0

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# Fixes bad `phpp` extensions to `php` in the wordpress file.
+exec { 'fix-wordpress':
+  command => 'bash -c "sed -i s/class-wp-locale.phpp/class-wp-locale.php/ \
+/var/www/html/wp-settings.php; service apache2 restart"',
+  path    => '/usr/bin:/usr/sbin:/bin'
+}
